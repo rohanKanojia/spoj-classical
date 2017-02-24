dcbd854ee0176c60a60c5215caff7bbedf416223
Rohan Kumar <rohankanojia420@gmail.com>
2017-02-25 01:17:19 +0530

    Alice Sieve

---
+++ AliceSieve.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  long long T;
+  cin >> T;
+  while(T--) {
+    long long N;
+    cin >> N;
+
+    cout << (N+1)/2 << endl;
+  }
+  return 0;
+}
